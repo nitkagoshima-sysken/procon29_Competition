0466655e4a7977cc8ee92c4ec5596f1663eb07d5
skytomo <tomo1kawa.fullerene@gmail.com>
2018-05-26 22:13:06 +0900

    [Visualizer][Change] Changed initialize procon in class Form1

--- procon29_disp/Form1.cs
+++ procon29_disp/Form1.cs
@@ -13,6 +13,9 @@ namespace procon29_disp
     public partial class Form1 : Form
     {
         Procon29_Display procon = new Procon29_Display();
+        /// <summary>
+        /// Form1
+        /// </summary>
         public Form1()
         {
             InitializeComponent();
@@ -20,35 +23,26 @@ namespace procon29_disp
             this.pictureBox1.MouseMove += new MouseEventHandler(mouseMove);
             this.Resize += new System.EventHandler(this.Form1_Resize);
 
-            var pointarray = new int[,] {
-                { -6, 15,  0,  7,  0, -1, 13, -8, -7, -7,  2, -3, },
-                {  8,  1, -5,  0, -2, -8, 10, -3,-15, 14, -4, -3, },
-                {-15, 14, -4, -3, -8, -3, -7, -5,-11,-16, -9, -1, },
-                { 15,  1, 14, 10,-11, 11, -2,  8,  6, 11,  9, -5, },
-                {-11,-16,  0,  8,-10, -8,-10,  5,  4,  5,  7,-14, },
-                {-13, -3, 16, -5,  6, 12, -3, -5,  0,  1, 16,-16, },
-                {-13, -3, 16, -5,  6, 12, -3, -5,  0,  1, 16,-16, },
-                {-11,-16,  0,  8,-10, -8,-10,  5,  4,  5,  7,-14, },
-                { 15,  1, 14, 10,-11, 11, -2,  8,  6, 11,  9, -5, },
-                {-15, 14, -4, -3, -8, -3, -7, -5,-11,-16, -9, -1, },
-                {  8,  1, -5,  0, -2, -8, 10, -3,-15, 14, -4, -3, },
-                { -6, 15,  0,  7,  0, -1, 13, -8, -7, -7,  2, -3, },
-                //{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, },
-                //{ 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, },
-                //{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, },
-                //{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, },
-                //{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, },
-                //{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, },
-                //{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, },
-                //{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, },
-                //{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, },
-                //{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, },
-                //{ 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, },
-                //{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, }
-            };
+            procon = new Procon29_Display(
+                field: new int[,] {
+                    { -6, 15,  0,  7,  0, -1, 13, -8, -7, -7,  2, -3, },
+                    {  8,  1, -5,  0, -2, -8, 10, -3,-15, 14, -4, -3, },
+                    {-15, 14, -4, -3, -8, -3, -7, -5,-11,-16, -9, -1, },
+                    { 15,  1, 14, 10,-11, 11, -2,  8,  6, 11,  9, -5, },
+                    {-11,-16,  0,  8,-10, -8,-10,  5,  4,  5,  7,-14, },
+                    {-13, -3, 16, -5,  6, 12, -3, -5,  0,  1, 16,-16, },
+                    {-13, -3, 16, -5,  6, 12, -3, -5,  0,  1, 16,-16, },
+                    {-11,-16,  0,  8,-10, -8,-10,  5,  4,  5,  7,-14, },
+                    { 15,  1, 14, 10,-11, 11, -2,  8,  6, 11,  9, -5, },
+                    {-15, 14, -4, -3, -8, -3, -7, -5,-11,-16, -9, -1, },
+                    {  8,  1, -5,  0, -2, -8, 10, -3,-15, 14, -4, -3, },
+                    { -6, 15,  0,  7,  0, -1, 13, -8, -7, -7,  2, -3, },
+                },
+                initPositionOfFilstAgentOfFirstTeam: new Point(11, 3),
+                initPositionOfSecondAgentOfFirstTeam: new Point(11, 9),
+                initPositionOfFilstAgentOfLastTeam: new Point(0, 3),
+                initPositionOfSecondAgentOfLastTeam: new Point(0, 9));
 
-           
-            procon = new Procon29_Display(pointarray);
             procon.Message = "This is Procon29 Visualizar, okay?\n";
             procon.PointMapCheck();
             procon.Show(pictureBox1);
@@ -56,7 +50,6 @@ namespace procon29_disp
             messageBox.Text += procon.Message;
             messageBox.Select(messageBox.Text.Length, 0);
 
-            
         }
 
         //Resizeイベントハンドラ
