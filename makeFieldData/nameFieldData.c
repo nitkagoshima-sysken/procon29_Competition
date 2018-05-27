2f8155301de37fe4e2ad806eec2d07e0f492070d
SeitaHigashi <higashi110902@gmail.com>
2018-05-28 00:05:03 +0900

    makeFieldData version 0.1

---
+++ makeFieldData/nameFieldData.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <string.h>
+
+#include "nameFieldData.h"
+
+int main(int argc,char* argv[]){
+  if(argc < 5){
+    fprintf(stderr,"入力データが不足しています。\n");
+    fprintf(stderr,"%s <vertical> <side> <type> <outfilename>\n",argv[0]);
+    fprintf(stderr,"type = 1:左右対称 2:上下対称 3:上下左右対称\n");
+    exit(1);
+  }
+
+  int vertical = atoi(argv[1]);
+  int side = atoi(argv[2]);
+  int type = atoi(argv[3]);
+
+  int data[12][12];
+  int i,j;
+  int point;
+
+  FILE *fp;
+
+  srand((unsigned)time(NULL));
+
+  if(type = 1){
+    for(i = 0;i<vertical;i++){
+      for(j = 0;j<=side/2;j++){
+        point = (rand()%33)-16;
+        data[i][j] = point;
+        data[i][side-1-j] = point;
+      }
+    }
+  }
+
+  fp = fopen(argv[4],"w");
+
+  fprintf(fp,"%d %d:",vertical,side);
+
+  for(i = 0;i < vertical;i++){
+    for(j = 0;j < side;j++){
+      fprintf(fp,"%d ",data[i][j]);
+    }
+    fprintf(fp,":");
+  }
+
+  fprintf(fp,"%d %d:%d %d:",rand()%vertical-1,rand()%side-1,rand()%vertical-1,rand()%side-1);
+
+  fclose(fp);
+
+  return 0;
+}
