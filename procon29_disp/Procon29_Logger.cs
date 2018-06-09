3e260cc40d42048f21e9ffd3aa80cf21ba6f0d3e
skytomo <tomo1kawa.fullerene@gmail.com>
2018-06-09 09:30:23 +0900

    [Visualizer][Add] Implemented class Procon29_Logger

--- procon29_disp/Procon29_Logger.cs
+++ procon29_disp/Procon29_Logger.cs
@@ -1,12 +1,34 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace procon29_disp
 {
     class Procon29_Logger
     {
+        private RichTextBox richTextBox;
+
+        public RichTextBox RichTextBox { get => richTextBox; set => richTextBox = value; }
+
+        public Procon29_Logger(RichTextBox richTextBox)
+        {
+            RichTextBox = richTextBox;
+        }
+
+        public void Write(Color color, string text)
+        {
+            RichTextBox.SelectionColor = color;
+            RichTextBox.SelectedText = text;
+        }
+
+        public void WriteLine(Color color, string text)
+        {
+            RichTextBox.SelectionColor = color;
+            RichTextBox.SelectedText = text + "\n";
+        }
     }
 }
