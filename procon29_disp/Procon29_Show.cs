49437e7d557b0c5489aca5d93c55efa1bd173100
skytomo <tomo1kawa.fullerene@gmail.com>
2018-06-09 17:16:13 +0900

    [Visualizer][Change] Made TeamDesign.cs

--- procon29_disp/Procon29_Show.cs
+++ procon29_disp/Procon29_Show.cs
@@ -8,23 +8,6 @@ using System.Windows.Forms;
 
 namespace procon29_disp
 {
-    class TeamDesign
-    {
-        private string name;
-        private Color agentColor, areaColor;
-
-        public string Name { get => name; set => name = value; }
-        public Color AgentColor { get => agentColor; set => agentColor = value; }
-        public Color AreaColor { get => areaColor; set => areaColor = value; }
-
-        public TeamDesign(string name, Color agentColor, Color areaColor)
-        {
-            Name = name;
-            AgentColor = agentColor;
-            AreaColor = areaColor;
-        }
-    }
-
     class Procon29_Show
     {
         private Procon29_Calc procon29_Calc;
