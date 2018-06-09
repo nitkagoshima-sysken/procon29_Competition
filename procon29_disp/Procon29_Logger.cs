683f7feb6e5dd1ecd122a35a0992ac4c5c9a3bd1
skytomo <tomo1kawa.fullerene@gmail.com>
2018-06-09 10:03:29 +0900

    [Visualizer][Add] Wrote "XML Documentation Comment" in Procon29_Logger

--- procon29_disp/Procon29_Logger.cs
+++ procon29_disp/Procon29_Logger.cs
@@ -8,27 +8,51 @@ using System.Windows.Forms;
 
 namespace procon29_disp
 {
+    /// <summary>
+    /// Procon29におけるログの管理を行います。
+    /// </summary>
     class Procon29_Logger
     {
+        /// <summary>
+        /// ログとして書き込むリッチテキストを設定します
+        /// </summary>
         private RichTextBox richTextBox;
 
+        /// <summary>
+        /// ログとして書き込むリッチテキストを設定または取得します
+        /// </summary>
         public RichTextBox RichTextBox { get => richTextBox; set => richTextBox = value; }
 
+        /// <summary>
+        /// Procon29_Loggerを初期化します
+        /// </summary>
+        /// <param name="richTextBox">ログとして書き込むリッチテキストを設定します</param>
         public Procon29_Logger(RichTextBox richTextBox)
         {
             RichTextBox = richTextBox;
+            Console.WriteLine("");
         }
 
-        public void Write(Color color, string text)
+        /// <summary>
+        /// 指定した文字列を指定した色でログに書き込みます。
+        /// </summary>
+        /// <param name="color">文字色</param>
+        /// <param name="message">ログに書き込むメッセージ</param>
+        public void Write(Color color, string message)
         {
             RichTextBox.SelectionColor = color;
-            RichTextBox.SelectedText = text;
+            RichTextBox.SelectedText = message;
         }
 
-        public void WriteLine(Color color, string text)
+        /// <summary>
+        /// 指定した文字列を指定した色でログに書き込み、続けて現在の行終端記号を書き込みます。
+        /// </summary>
+        /// <param name="color">文字色</param>
+        /// <param name="message">ログに書き込むメッセージ</param>
+        public void WriteLine(Color color, string message)
         {
             RichTextBox.SelectionColor = color;
-            RichTextBox.SelectedText = text + "\n";
+            RichTextBox.SelectedText = message + "\n";
         }
     }
 }
