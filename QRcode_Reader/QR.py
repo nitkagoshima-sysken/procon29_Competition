11ac60e712363bce2706ee5816e5bcfac4a77a99
skytomo <tomo1kawa.fullerene@gmail.com>
2018-06-13 20:33:42 +0900

    Merge branch 'master' into skytomo

--- QRcode_Reader/QR.py
+++ QRcode_Reader/QR.py
@@ -2,15 +2,12 @@ from pyzbar.pyzbar import decode
 from PIL import Image
 import os
 
-# QRコード(QRcode.png)の指定
 image = input('File:')
-# QRコードの読取り
 data = decode(Image.open(image))
-# コード内容テキストファイル(output.txt)に出力
-f = open('output.txt','w')
-# utf-8にデコードして出力
-text = data[0][0].decode('utf-8', 'ignore')
-outt = text.replace(' ',',')
-f.write(outt)
-print(outt)
+f = open('output.csv','w')
+outt = data[0][0].decode('utf-8', 'ignore')
+outt = outt.split(':')
+for i in range(len(outt)-1):
+    f.write(outt[i].strip().replace(' ',',')+'\n')
+    print(outt[i].strip().replace(' ',','))
 f.close()
