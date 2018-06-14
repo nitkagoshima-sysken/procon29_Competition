4b8059fc29d2f2da529e9d4e1e25c20d6723b358
skytomo <tomo1kawa.fullerene@gmail.com>
2018-06-14 21:35:28 +0900

    [Visualizer][Add] Added in class Field

--- procon29_disp/Field.cs
+++ procon29_disp/Field.cs
@@ -103,5 +103,77 @@ namespace procon29_disp
             IsIndirectArea = new bool[2];
             IsAreaCheck = new bool[2];
         }
+
+        /// <summary>
+        /// CSV形式の文字列をListに変換します。
+        /// </summary>
+        /// <param name="str">変換する文字列</param>
+        /// <returns></returns>
+        public static List<int> CSVtoList(string str)
+        {
+
+            var list = new List<int>();
+
+            //前後の改行を削除しておく
+            str = str.Trim(new char[] { '\r', '\n' });
+            //1行のCSVから各フィールドを取得するための正規表現
+            System.Text.RegularExpressions.Regex regex =
+                new System.Text.RegularExpressions.Regex(
+                    @"(\d*,)+",
+                    System.Text.RegularExpressions.RegexOptions.None);
+
+            //1つの行からフィールドを取り出す
+            foreach (System.Text.RegularExpressions.Match match in regex.Matches(str))
+            {
+                for (int i = 0; i < match.Groups[0].Captures.Count; i++)
+                {
+                    string field = match.Groups[1].Captures[i].Value;
+                    //前後の空白を削除
+                    field = field.Trim();
+                    field = field.Replace(",", "");
+                    list.Add(int.Parse(field));
+                }
+            }
+            return list;
+        }
+
+        /// <summary>
+        /// 第1要素に高さと第2要素に幅のデータを含んだリストをFieldsに変換します。
+        /// </summary>
+        /// <param name="list">変換するリスト</param>
+        /// <returns></returns>
+        public static Field[,] ListWithHeightAndWidthToFields(List<int> list)
+        {
+            var height = list[0];
+            var width = list[1];
+            list.RemoveAt(0);
+            list.RemoveAt(0);
+            return ListToFields(list, height, width);
+        }
+
+        /// <summary>
+        /// リストをFieldsに変換します。
+        /// </summary>
+        /// <param name="list">変換するリスト</param>
+        /// <param name="height">フィールドの高さ</param>
+        /// <param name="width">フィールドの幅</param>
+        /// <returns></returns>
+        public static Field[,] ListToFields(List<int> list, int height, int width)
+        {
+            var fields = new Field[height, width];
+            for (int i = 0; i < list.Count; i++)
+            {
+                fields[i / width, i % width] = new Field();
+                fields[i / width, i % width].Point = list[i];
+            }
+            return fields;
+        }
+
+        /// <summary>
+        /// CSV形式の文字列をFieldsに変換します。
+        /// </summary>
+        /// <param name="str"></param>
+        /// <returns></returns>
+        public static Field[,] CSVToFields(string str) => ListWithHeightAndWidthToFields(CSVtoList(str));
     }
 }
