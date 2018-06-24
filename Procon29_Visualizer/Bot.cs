da486c05295896ebda575ac7eee442fc39446e22
skytomo <tomo1kawa.fullerene@gmail.com>
2018-06-24 23:39:46 +0900

    [Visualizer][Add] Bot.cs

---
+++ Procon29_Visualizer/Bot.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Procon29_Visualizer
+{
+    abstract class Bot
+    {
+        Calc calc;
+
+        internal abstract Calc Calc { get; set; }
+
+        Bot(Calc calc)
+        {
+            Calc = calc;
+        }
+    }
+}
