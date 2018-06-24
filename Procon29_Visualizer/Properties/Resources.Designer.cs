65499d8c6ac76a671544e5ff5fe8d9be081e6ee3
skytomo <tomo1kawa.fullerene@gmail.com>
2018-06-24 15:35:02 +0900

    [Visualizer][Change] Default namespace

--- Procon29_Visualizer/Properties/Resources.Designer.cs
+++ Procon29_Visualizer/Properties/Resources.Designer.cs
@@ -39,7 +39,7 @@ namespace Procon29_Visualizer.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("procon29_disp.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Procon29_Visualizer.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
