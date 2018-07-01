523a524219c656142005dca4ff11162725639147
skytomo <tomo1kawa.fullerene@gmail.com>
2018-07-01 13:32:28 +0900

    [Visualizer][Add] TeamArray.cs

---
+++ Visualizer/TeamArray.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nitkagoshima_sysken.Procon29.Visualizer
+{
+    class TeamArray
+    {
+        public bool[] Array { get; set; } = new bool[Enum.GetValues(typeof(Team)).Length];
+
+        public TeamArray() { }
+
+        public bool this[Team team, Agent agent]
+        {
+            set { Array[(int)team] = value; }
+            get { return Array[(int)team]; }
+        }
+    }
+}
