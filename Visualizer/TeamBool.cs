e169300615c00632c5c409dad53ac26419f9618b
skytomo <tomo1kawa.fullerene@gmail.com>
2018-07-01 15:01:00 +0900

    [Visualizer][Change] Renamed to TeamBool.cs

---
+++ Visualizer/TeamBool.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nitkagoshima_sysken.Procon29.Visualizer
+{
+    /// <summary>
+    /// チームごとの真偽を表します。
+    /// </summary>
+    public class TeamBool
+    {
+        bool[] Array { get; set; } = new bool[Enum.GetValues(typeof(Team)).Length];
+
+        /// <summary>
+        /// TeamBoolを設定します
+        /// </summary>
+        public TeamBool() { }
+
+        /// <summary>
+        /// チームごとの真偽を表します。
+        /// </summary>
+        /// <param name="team">対象となるチーム</param>
+        /// <returns></returns>
+        public bool this[Team team]
+        {
+            set { Array[(int)team] = value; }
+            get { return Array[(int)team]; }
+        }
+    }
+}
