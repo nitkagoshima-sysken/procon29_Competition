2628e60847907f178b83d12e3c1f7016dce1daa3
SeitaHigashi <higashi110902@gmail.com>
2018-09-01 16:12:36 +0900

    [add] NettaBot

---
+++ NettaBot/NettaBot.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+using nitkagoshima_sysken.Procon29.Visualizer;
+
+namespace nitkagoshima_sysken.Procon29.NettaBot
+{
+    class NettaBot : Bot.Bot
+    {
+        public NettaBot() : base() { }
+
+        public override AgentActivityData[] Answer()
+        {
+            var result = new AgentActivityData[2];
+
+            result[0] = new AgentActivityData(AgentStatusCode.RequestNotToDoAnything);
+            result[1] = new AgentActivityData(AgentStatusCode.RequestNotToDoAnything);
+
+            return result;
+        }
+    }
+}
