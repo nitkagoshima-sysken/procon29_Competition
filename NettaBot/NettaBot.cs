b2bf0a8669c0497439ca48ef0cbe7ea6de2c8600
SeitaHigashi <higashi110902@gmail.com>
2018-09-01 16:56:28 +0900

    [add] IsOdd

--- NettaBot/NettaBot.cs
+++ NettaBot/NettaBot.cs
@@ -12,10 +12,25 @@ namespace nitkagoshima_sysken.Procon29.NettaBot
         {
             var result = new AgentActivityData[2];
 
+            foreach(var agent in Calc.Agents)
+            {
+
+            }
+
             result[0] = new AgentActivityData(AgentStatusCode.RequestNotToDoAnything);
             result[1] = new AgentActivityData(AgentStatusCode.RequestNotToDoAnything);
 
             return result;
+        }
+
+        private Boolean IsOdd()
+        {
+            var sum = 0; ;
+            foreach(var cell in Calc.Field.ToList())
+            {
+                sum = sum + (cell.Coordinate.X + cell.Coordinate.Y) % 2 != 0 ? cell.Point : -cell.Point;
+            }
+            return sum > 0;
         }
     }
 }
