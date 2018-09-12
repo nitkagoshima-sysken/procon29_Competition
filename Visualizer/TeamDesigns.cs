d7f9f4bf552b9f49a726011e2373bd83ed4c9469
skytomo <tomo1kawa.fullerene@gmail.com>
2018-09-12 19:57:11 +0900

    [Visualizer][Add] TeamDesigns.cs

---
+++ Visualizer/TeamDesigns.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nitkagoshima_sysken.Procon29.Visualizer
+{
+    class TeamDesigns : IEnumerable<TeamDesign>
+    {
+        TeamDesign[] Array { get; set; } = new TeamDesign[Enum.GetValues(typeof(Team)).Length];
+
+        /// <summary>
+        /// 列挙します。
+        /// </summary>
+        /// <returns>列挙されたエージェント</returns>
+        public IEnumerator<TeamDesign> GetEnumerator()
+        {
+            foreach (var item in Array)
+            {
+                yield return item;
+            }
+        }
+
+        /// <summary>
+        /// 列挙します
+        /// </summary>
+        /// <returns>列挙されたセル</returns>
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            foreach (TeamDesign item in Array)
+            {
+                yield return item;
+            }
+        }
+
+        /// <summary>
+        /// 列挙します
+        /// </summary>
+        /// <returns>列挙されたセル</returns>
+        IEnumerator<TeamDesign> IEnumerable<TeamDesign>.GetEnumerator()
+        {
+            foreach (TeamDesign item in Array)
+            {
+                yield return item;
+            }
+        }
+
+        /// <summary>
+        /// TeamDesignsを初期化します。
+        /// </summary>
+        public TeamDesigns()
+        {
+            Array[0] = new TeamDesign("Orange", Color.DarkOrange, Color.DarkOrange);
+            Array[1] = new TeamDesign("Lime", Color.LimeGreen, Color.LimeGreen);
+        }
+
+        /// <summary>
+        /// エージェントを取得または設定します
+        /// </summary>
+        /// <param name="team">対象となる所属チーム</param>
+        /// <param name="agent">対象となるエージェント番号</param>
+        /// <returns></returns>
+        public TeamDesign this[Team team]
+        {
+            set { Array[(int)team] = value; }
+            get { return Array[(int)team]; }
+        }
+    }
+}
