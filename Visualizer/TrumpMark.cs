c980c926d2d720bad6e271da5881bec61b606215
skytomo <tomo1kawa.fullerene@gmail.com>
2018-09-24 18:24:54 +0900

    [Visualizer][Delete] Unnecessary "using" namespace

--- Visualizer/TrumpMark.cs
+++ Visualizer/TrumpMark.cs
@@ -1,12 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace nitkagoshima_sysken.Procon29.Visualizer
+﻿namespace nitkagoshima_sysken.Procon29.Visualizer
 {
-    class TrumpMark
+    enum TrumpMark
     {
+        Heart,
+        Diamond,
+        Club,
+        Spade,
     }
 }
