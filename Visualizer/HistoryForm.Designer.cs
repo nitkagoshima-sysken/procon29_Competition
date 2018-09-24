1ce699d684767bcbd7fea61e265b46fcad08eb45
skytomo <tomo1kawa.fullerene@gmail.com>
2018-09-24 17:10:52 +0900

    [Visualizer][Fix] Procon29 Visualizer History

--- Visualizer/HistoryForm.Designer.cs
+++ Visualizer/HistoryForm.Designer.cs
@@ -28,10 +28,18 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // HistoryForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "HistoryForm";
+            this.Name = "HistoryForm";
+            this.Text = "Procon29 Visualizer History";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
