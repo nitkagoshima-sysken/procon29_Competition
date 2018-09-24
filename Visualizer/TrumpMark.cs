75350846bf16c326e1bc4263599e6e7933c3dba6
skytomo <tomo1kawa.fullerene@gmail.com>
2018-09-24 18:23:21 +0900

    [Visualizer][Add] TrumpMark.cs

---
+++ Visualizer/TrumpMark.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nitkagoshima_sysken.Procon29.Visualizer
+{
+    class TrumpMark
+    {
+    }
+}
