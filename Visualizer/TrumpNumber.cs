2a3d458084e6ddeb31488f5dcfd7ec77353615bb
skytomo <tomo1kawa.fullerene@gmail.com>
2018-09-24 18:33:35 +0900

    [Visualizer][Add] TrumpNumber.cs

---
+++ Visualizer/TrumpNumber.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nitkagoshima_sysken.Procon29.Visualizer
+{
+    enum TrumpNumber
+    {
+        Ace,
+        Two,
+        Three,
+        Four,
+        Five,
+        Six,
+        Seven,
+        Eight,
+        Nine,
+        Jack,
+        Queen,
+        King,
+        Joker,
+    }
+}
