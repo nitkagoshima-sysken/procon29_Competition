789096f3bc53842aa5a575e80b5383482c7dea25
skytomo <tomo1kawa.fullerene@gmail.com>
2018-09-24 17:09:36 +0900

    [Visualizer][Add] Procon29 Visualizer History

---
+++ Visualizer/HistoryForm.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace nitkagoshima_sysken.Procon29.Visualizer
+{
+    public partial class HistoryForm : Form
+    {
+        public HistoryForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
