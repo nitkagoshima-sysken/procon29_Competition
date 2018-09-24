e1ccf88b83456a10eedd7aea7d3cbb2b4edeadb6
skytomo <tomo1kawa.fullerene@gmail.com>
2018-09-24 18:28:22 +0900

    [Visualizer][Add] Wrote "XML Documentation Comment"

--- Visualizer/TrumpMark.cs
+++ Visualizer/TrumpMark.cs
@@ -1,10 +1,25 @@
 ﻿namespace nitkagoshima_sysken.Procon29.Visualizer
 {
+    /// <summary>
+    /// トランプのマークを表します。
+    /// </summary>
     enum TrumpMark
     {
+        /// <summary>
+        /// ハートを表します。
+        /// </summary>
         Heart,
+        /// <summary>
+        /// ダイアを表します。
+        /// </summary>
         Diamond,
+        /// <summary>
+        /// クラブを表します。
+        /// </summary>
         Club,
+        /// <summary>
+        /// スペードを表します。
+        /// </summary>
         Spade,
     }
 }
