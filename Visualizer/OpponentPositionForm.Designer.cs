514a8139065ae2188a09a97dc27f4cea9c836d84
skytomo <tomo1kawa.fullerene@gmail.com>
2018-10-14 13:13:06 +0900

    [Visualizer][Add] OK Button

--- Visualizer/OpponentPositionForm.Designer.cs
+++ Visualizer/OpponentPositionForm.Designer.cs
@@ -72,6 +72,7 @@
             this.button1.TabIndex = 6;
             this.button1.Text = "OK";
             this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // OpponentPosition1X
             // 
--- Visualizer/OpponentPositionForm.cs
+++ Visualizer/OpponentPositionForm.cs
@@ -16,5 +16,10 @@ namespace nitkagoshima_sysken.Procon29.Visualizer
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Hide();
+        }
     }
 }
