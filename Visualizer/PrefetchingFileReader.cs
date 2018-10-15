bb291a2c890a6905b21b207b9a451bc503329532
skytomo <tomo1kawa.fullerene@gmail.com>
2018-10-15 23:15:40 +0900

    [Visualizer][Add] in PrefetchingFileReader.cs

--- Visualizer/PrefetchingFileReader.cs
+++ Visualizer/PrefetchingFileReader.cs
@@ -1,9 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.Drawing;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace nitkagoshima_sysken.Procon29.Visualizer
 {
