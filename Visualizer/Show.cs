9ce63b7890abc49ca2d693d0bbb02ea07442a465
skytomo <tomo1kawa.fullerene@gmail.com>
2018-10-17 23:53:48 +0900

    [Visualizer][Delete] in Show.cs

--- Visualizer/Show.cs
+++ Visualizer/Show.cs
@@ -29,20 +29,7 @@ namespace nitkagoshima_sysken.Procon29.Visualizer
         /// </summary>
         protected int CellWidth { get; set; }
 
-        /// <summary>
-        /// 描画するときの色を設定または取得します。
-        /// </summary>
-        public TeamDesigns TeamDesigns { get; set; } = new TeamDesigns();
-
         public ClickField ClickField { get; set; }
-
-
-        // not under
-
-        /// <summary>
-        /// 描画するときの色を設定または取得します。
-        /// </summary>
-        public TeamDesign[] TeamDesign { get; set; }
 
         /// <summary>
         /// 描画する対象となるPictureBoxを設定または取得します。
@@ -50,34 +37,9 @@ namespace nitkagoshima_sysken.Procon29.Visualizer
         public PictureBox PictureBox { get; set; }
 
         /// <summary>
-        /// 背景をどのように塗りつぶすか設定または取得します。
-        /// </summary>
-        public SolidBrush BackGroundSolidBrush { get; set; } = new SolidBrush(Color.FromArgb(48, 48, 48));
-
-        /// <summary>
-        /// 選択したフィールドをどのように塗りつぶすか設定または取得します。
-        /// </summary>
-        public SolidBrush SelectSolidBrush { get; set; } = new SolidBrush(Color.FromArgb(50, Color.DarkGray));
-
-        /// <summary>
-        /// クリックしたフィールドをどのように塗りつぶすか設定または取得します。
-        /// </summary>
-        public SolidBrush ClickedSolidBrush { get; set; } = new SolidBrush(Color.FromArgb(100, Color.SkyBlue));
-
-        /// <summary>
-        /// フォントの設定または取得します。
-        /// </summary>
-        public Font PointFont { get; set; }
-
-        /// <summary>
         /// クリックしたときのフィールドの場所の設定または取得します。
         /// </summary>
         public Point ClickedField { get; set; }
-
-        /// <summary>
-        /// ログを書き込むためのProcon29_Loggerを設定または取得します。
-        /// </summary>
-        internal Logger Procon29_Logger { get; set; }
 
         public Team SelectedTeam { get; set; }
         public AgentNumber SelecetedAgent { get; set; }
@@ -133,7 +95,7 @@ namespace nitkagoshima_sysken.Procon29.Visualizer
         public Show(Calc procon29_Calc, TeamDesign[] teamDesigns, PictureBox pictureBox)
         {
             Calc = procon29_Calc;
-            TeamDesign = teamDesigns;
+            //TeamDesign = teamDesigns;
             PictureBox = pictureBox;
 
             // ResourceManagerを取得する
@@ -169,9 +131,7 @@ namespace nitkagoshima_sysken.Procon29.Visualizer
         public Show(Calc procon29_Calc, TeamDesign[] teamDesigns, PictureBox pictureBox, Logger procon29_Logger)
         {
             Calc = procon29_Calc;
-            TeamDesign = teamDesigns;
             PictureBox = pictureBox;
-            Procon29_Logger = procon29_Logger;
         }
 
         /// <summary>
@@ -182,7 +142,7 @@ namespace nitkagoshima_sysken.Procon29.Visualizer
         /// <summary>
         /// フィールドを描画するオブジェクトを設定または取得します。
         /// </summary>
-        public DrawField drawField { get; set; }
+        public DrawField DrawField { get; set; }
 
         /// <summary>
         /// PictureBoxを新たに生成します。
@@ -200,9 +160,9 @@ namespace nitkagoshima_sysken.Procon29.Visualizer
 
             var cursor = CursorPosition(PictureBox);
 
-            drawField = new DrawField(Calc, canvas);
-            drawField.AgentsActivityData = agentActivityData;
-            drawField.Draw(cursor);
+            DrawField = new DrawField(Calc, canvas);
+            DrawField.AgentsActivityData = agentActivityData;
+            DrawField.Draw(cursor);
             precursor = cursor;
             try
             {
@@ -210,12 +170,12 @@ namespace nitkagoshima_sysken.Procon29.Visualizer
                 {
                     if (ClickField.ClickedAgent == agent && cursor.ChebyshevDistance(agent.Position) == 1)
                     {
-                        drawField.DrawArrow(agent.Team, agent.Position, cursor);
+                        DrawField.DrawArrow(agent.Team, agent.Position, cursor);
                         break;
                     }
                     else if (cursor.ChebyshevDistance(agent.Position) == 1 && cursor.ChebyshevDistance(ClickField.ClickedAgent.Position) != 1)
                     {
-                        drawField.DrawArrow(agent.Team, agent.Position, cursor);
+                        DrawField.DrawArrow(agent.Team, agent.Position, cursor);
                         break;
                     }
                 }
@@ -245,9 +205,9 @@ namespace nitkagoshima_sysken.Procon29.Visualizer
 
             var cursor = CursorPosition(PictureBox);
 
-            drawField = new DrawField(Calc, canvas);
-            drawField.AgentsActivityData = agentActivityData;
-            drawField.Draw(turn, cursor);
+            DrawField = new DrawField(Calc, canvas);
+            DrawField.AgentsActivityData = agentActivityData;
+            DrawField.Draw(turn, cursor);
             precursor = cursor;
             try
             {
@@ -255,12 +215,12 @@ namespace nitkagoshima_sysken.Procon29.Visualizer
                 {
                     if (ClickField.ClickedAgent == agent && cursor.ChebyshevDistance(agent.Position) == 1)
                     {
-                        drawField.DrawArrow(agent.Team, agent.Position, cursor);
+                        DrawField.DrawArrow(agent.Team, agent.Position, cursor);
                         break;
                     }
                     else if (cursor.ChebyshevDistance(agent.Position) == 1 && cursor.ChebyshevDistance(ClickField.ClickedAgent.Position) != 1)
                     {
-                        drawField.DrawArrow(agent.Team, agent.Position, cursor);
+                        DrawField.DrawArrow(agent.Team, agent.Position, cursor);
                         break;
                     }
                 }
@@ -416,7 +376,6 @@ namespace nitkagoshima_sysken.Procon29.Visualizer
                 y: pictureBoxCursorPosition.Y / ((fieldHeight <= 0) ? 1 : fieldHeight));
         }
 
-
         /// <summary>
         /// Form1内でキーを押したときに実行されます。
         /// </summary>
@@ -426,82 +385,7 @@ namespace nitkagoshima_sysken.Procon29.Visualizer
         {
             e.SuppressKeyPress = true;
             ClickField.PushKey(e.KeyCode);
-            
-
-            //var current = Calc.Agents[SelectedTeam, SelecetedAgent].Position;
-            //var next = agentActivityData[SelectedTeam, SelecetedAgent];
-
-            //Console.WriteLine(e.KeyCode);
-            //try
-            //{
-            //    switch (e.KeyCode)
-            //    {
-            //        case Keys.Q:
-            //            SelectedTeam = Team.A;
-            //            SelecetedAgent = AgentNumber.One;
-            //            break;
-            //        case Keys.W:
-            //            SelectedTeam = Team.A;
-            //            SelecetedAgent = AgentNumber.Two;
-            //            break;
-            //        case Keys.E:
-            //            SelectedTeam = Team.B;
-            //            SelecetedAgent = AgentNumber.One;
-            //            break;
-            //        case Keys.R:
-            //            SelectedTeam = Team.B;
-            //            SelecetedAgent = AgentNumber.Two;
-            //            break;
-            //        case Keys.NumPad1:
-            //            next.Destination = current + Arrow.DownLeft;
-            //            break;
-            //        case Keys.NumPad2:
-            //            next.Destination = current + Arrow.Down;
-            //            break;
-            //        case Keys.NumPad3:
-            //            next.Destination = current + Arrow.DownRight;
-            //            break;
-            //        case Keys.NumPad4:
-            //            next.Destination = current + Arrow.Left;
-            //            break;
-            //        case Keys.NumPad6:
-            //            next.Destination = current + Arrow.Right;
-            //            break;
-            //        case Keys.NumPad7:
-            //            next.Destination = current + Arrow.UpLeft;
-            //            break;
-            //        case Keys.NumPad8:
-            //            next.Destination = current + Arrow.Up;
-            //            break;
-            //        case Keys.NumPad9:
-            //            next.Destination = current + Arrow.UpRight;
-            //            break;
-            //    }
-
-            //    current = Calc.Agents[SelectedTeam, SelecetedAgent].Position;
-            //    next = agentActivityData[SelectedTeam, SelecetedAgent];
-
-            //    ClickedField = current;
-            //    if (next.Destination.X < 0 ||
-            //        next.Destination.Y < 0 ||
-            //        next.Destination.X >= Calc.Field.Width ||
-            //        next.Destination.Y >= Calc.Field.Height)
-            //    {
-            //        next.Destination = current;
-            //        throw new Exception();
-            //    }
-            //    else if (current != next.Destination)
-            //    {
-            //        KeyDownShow();
-            //    }
-            //}
-            //catch (Exception)
-            //{
-            //    MessageBox.Show("不正なキー入力です。", "エラー", MessageBoxButtons.OK, MessageBoxIcon.Error);
-            //}
             Showing();
         }
-
-
     }
 }
