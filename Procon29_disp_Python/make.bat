dedc1d21216fed53a3d6447acec7d0dfc8e1bd48
Glaz Egy <rei.izayoi.0804@gmail.com>
2018-10-17 23:32:18 +0900

    Add DLL connect

--- Procon29_disp_Python/make.bat
+++ Procon29_disp_Python/make.bat
@@ -1,3 +1,3 @@
 cd ..\..\Compile_python\
-pyinstaller ..\procon29_Competition\Procon29_disp_Python\Learn.py --onefile -n Learn
+pyinstaller ..\procon29_Competition\Procon29_disp_Python\hydro_go_bot.py --onefile -n HGbot
 cd ..\procon29_Competition\Procon29_disp_Python
