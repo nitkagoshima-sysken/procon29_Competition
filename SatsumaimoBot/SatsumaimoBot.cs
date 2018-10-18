3aec7c370e66875dad2bf2f0eb9e3b3e48d86bb2
skytomo <tomo1kawa.fullerene@gmail.com>
2018-10-18 23:21:08 +0900

    [SatsumaimoBot][Change] Release Satsumaimo Bot 1.64

--- SatsumaimoBot/SatsumaimoBot.cs
+++ SatsumaimoBot/SatsumaimoBot.cs
@@ -22,23 +22,65 @@ namespace nitkagoshima_sysken.Procon29.SatsumaimoBot
             {
                 foreach (Arrow arrow2 in Enum.GetValues(typeof(Arrow)))
                 {
-                    var test = new AgentActivityData[2]
+                    if (Calc.Field.CellExist(Calc.Agents[OurTeam, AgentNumber.One].Position + arrow1) &&
+                        Calc.Field.CellExist(Calc.Agents[OurTeam, AgentNumber.Two].Position + arrow2))
                     {
-                        new AgentActivityData() { },
-                        new AgentActivityData() { }
-                    };
-                    AgentActivityData[0].AgentStatusData = MoveOrRemove(Calc.Agents[OurTeam, AgentNumber.One], arrow1);
-                    AgentActivityData[0].Destination = Calc.Agents[OurTeam, AgentNumber.One].Position + arrow1;
-                    AgentActivityData[1].AgentStatusData = MoveOrRemove(Calc.Agents[OurTeam, AgentNumber.Two], arrow2);
-                    AgentActivityData[1].Destination = Calc.Agents[OurTeam, AgentNumber.Two].Position + arrow2;
-                    var c = Simulate(OurTeam, AgentActivityData);
-                    var point = c.Field.TotalPoint(OurTeam) - c.Field.TotalPoint(OurTeam.Opponent());
-                    if (maxpoint < point)
-                    {
-                        maxpoint = point;
+                        var test = new AgentActivityData[2]
+                        {
+                            new AgentActivityData()
+                            {
+                                AgentStatusData = MoveOrRemove(Calc.Agents[OurTeam, AgentNumber.One], arrow1),
+                                Destination = Calc.Agents[OurTeam, AgentNumber.One].Position + arrow1
+                            },
+                            new AgentActivityData()
+                            {
+                                AgentStatusData = MoveOrRemove(Calc.Agents[OurTeam, AgentNumber.Two], arrow2),
+                                Destination = Calc.Agents[OurTeam, AgentNumber.Two].Position + arrow2
+                            },
+                        };
+                        var c = Simulate(OurTeam, test);
+                        var point = c.Field.TotalPoint(OurTeam) - c.Field.TotalPoint(OurTeam.Opponent());
+                        if (maxpoint < point)
+                        {
+                            var pastcalc = Calc.History[(Calc.Turn - 1 > 0) ? Calc.Turn - 1 : 0];
+                            if ((pastcalc.AgentsActivityData[OurTeam, AgentNumber.One].AgentStatusData != AgentStatusCode.NotDoneAnything &&
+                                pastcalc.AgentsActivityData[OurTeam, AgentNumber.One].AgentStatusData.ToRequest() != test[0].AgentStatusData) ||
+                                pastcalc.AgentsActivityData[OurTeam, AgentNumber.One].Destination != test[0].Destination ||
+                                (pastcalc.AgentsActivityData[OurTeam, AgentNumber.Two].AgentStatusData != AgentStatusCode.NotDoneAnything &&
+                                pastcalc.AgentsActivityData[OurTeam, AgentNumber.Two].AgentStatusData.ToRequest() != test[1].AgentStatusData) ||
+                                pastcalc.AgentsActivityData[OurTeam, AgentNumber.Two].Destination != test[1].Destination)
+                            {
+                                maxpoint = point;
+                            }
+                            if ((pastcalc.AgentsActivityData[OurTeam, AgentNumber.One].AgentStatusData != AgentStatusCode.NotDoneAnything &&
+                                pastcalc.AgentsActivityData[OurTeam, AgentNumber.One].AgentStatusData.ToRequest() != test[0].AgentStatusData) ||
+                                pastcalc.AgentsActivityData[OurTeam, AgentNumber.One].Destination != test[0].Destination)
+                            {
+                                AgentActivityData[0].AgentStatusData = test[0].AgentStatusData;
+                                AgentActivityData[0].Destination = test[0].Destination;
+                            }
+                            else
+                            {
+                                Log.WriteLine("error yesterday crash one");
+                            }
+                            if ((pastcalc.AgentsActivityData[OurTeam, AgentNumber.Two].AgentStatusData != AgentStatusCode.NotDoneAnything &&
+                                pastcalc.AgentsActivityData[OurTeam, AgentNumber.Two].AgentStatusData.ToRequest() != test[1].AgentStatusData) ||
+                                pastcalc.AgentsActivityData[OurTeam, AgentNumber.Two].Destination != test[1].Destination)
+                            {
+                                AgentActivityData[1].AgentStatusData = test[1].AgentStatusData;
+                                AgentActivityData[1].Destination = test[1].Destination;
+                            }
+                            else
+                            {
+                                Log.WriteLine("error yesterday crash two");
+                            }
+                        }
                     }
                 }
             }
+            //System.Random r = new System.Random(1000);
+
+
             return AgentActivityData;
         }
 
