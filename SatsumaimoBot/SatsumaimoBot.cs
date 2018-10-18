da6d47241e69173a4ed5ab0df6a185214bead85e
skytomo <tomo1kawa.fullerene@gmail.com>
2018-10-18 23:56:22 +0900

    [SatsumaimoBot][Add]

---
+++ SatsumaimoBot/SatsumaimoBot.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using nitkagoshima_sysken.Procon29.Visualizer;
+
+namespace nitkagoshima_sysken.Procon29.SatsumaimoBot
+{
+    public class SatsumaimoBot : KaraimoBot.KaraimoBot
+    {
+        public override AgentActivityData[] Answer()
+        {
+            var ret = base.Answer();
+            foreach (var agent in Calc.Agents[OurTeam])
+            {
+                if (ret[(int)agent.AgentNumber].AgentStatusData == AgentStatusCode.RequestNotToDoAnything)
+                {
+                    System.Random r = new System.Random();
+                    Arrow arrow = (Arrow)r.Next(0, 7);
+
+                    while (true)
+                    {
+                        if (Calc.Field.IsMovable(agent, arrow))
+                        {
+                            ret[(int)agent.AgentNumber].AgentStatusData = AgentStatusCode.RequestMovement;
+                            ret[(int)agent.AgentNumber].Destination = agent.Position + arrow;
+                            break;
+                        }
+                        else if (Calc.Field.IsRemovableOpponentTile(agent, arrow))
+                        {
+                            ret[(int)agent.AgentNumber].AgentStatusData = AgentStatusCode.RequestRemovementOpponentTile;
+                            ret[(int)agent.AgentNumber].Destination = agent.Position + arrow;
+                            break;
+                        }
+                    }
+                }
+            }
+            return ret;
+        }
+    }
+}
