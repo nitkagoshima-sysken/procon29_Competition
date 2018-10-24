2dcc179ded055772aa880bd2cca2cf9f1f09e973
Seita Higashi <higashi110902@gmail.com>
2018-10-24 14:54:02 +0900

    BestHand obsolete

--- HeBot/HeBot.cs
+++ HeBot/HeBot.cs
@@ -17,33 +17,18 @@ namespace nitkagoshima_sysken.Procon29.HeBot
 
     class HeBot : Bot.Bot
     {
-        public HeBot() : base()
-        {
-        }
-        private Agent agentOne;
-        private Agent agentTwo;
+        public HeBot() : base(){}
 
         public override AgentActivityData[] Answer()
         {
-            agentOne = Calc.Agents[OurTeam, AgentNumber.One];
-            agentTwo = Calc.Agents[OurTeam, AgentNumber.Two];
             var result = new AgentActivityData[2];
-
-            result = BestHand(Calc, 1).AgentActivityData;
-
-
-            return result;
-        }
-
-        private ReturnStruct BestHand(Calc calc, int depth)
-        {
             var maxpoint = int.MinValue;
             var agentActivityData = new AgentActivityData[2];
-            var result = new AgentActivityData[2];
+
+            result = BestHand(Calc, 1).AgentActivityData;
             foreach (Arrow arrowOne in Enum.GetValues(typeof(Arrow)))
             {
-                var destinationOne = calc.Agents[OurTeam, AgentNumber.One].Position + arrowOne;
-                if (!calc.Field.CellExist(destinationOne)) continue;
+                var destinationOne = calc.Agents[OurTeam, AgentNumber.One].Position + arrowOne; if (!calc.Field.CellExist(destinationOne)) continue;
 
                 foreach (Arrow arrowTwo in Enum.GetValues(typeof(Arrow)))
                 {
@@ -54,36 +39,20 @@ namespace nitkagoshima_sysken.Procon29.HeBot
                     agentActivityData[(int)AgentNumber.Two] = MoveOrRemoveTile(destinationTwo);
                     var c = calc.Simulate(OurTeam, agentActivityData);
                     foreach (var item in agentActivityData) item.AgentStatusData.ToRequest();
-                    if (depth <= 1)
+                    if (maxpoint < (c.Field.TotalPoint(OurTeam) - c.Field.TotalPoint(OurTeam.Opponent())))
                     {
-                        if (maxpoint < (c.Field.TotalPoint(OurTeam) - c.Field.TotalPoint(OurTeam.Opponent())))
-                        {
-                            maxpoint = c.Field.TotalPoint(OurTeam) - c.Field.TotalPoint(OurTeam.Opponent());
-                            result = agentActivityData.DeepClone();
-                        }
-                    }
-                    else
-                    {
-                        var bestHand = BestHand(c, depth - 1);
-                        if (maxpoint < bestHand.point)
-                        {
-                            maxpoint = bestHand.point;
-                            result = agentActivityData.DeepClone();
-                        }
+                        maxpoint = c.Field.TotalPoint(OurTeam) - c.Field.TotalPoint(OurTeam.Opponent());
+                        result = agentActivityData.DeepClone();
                     }
                 }
             }
-            return new ReturnStruct(maxpoint, result);
-        }
 
+            return result;
+        }
         private AgentActivityData MoveOrRemoveTile(Coordinate coordinate)
         {
-            if (Calc.Field[coordinate].IsTileOn[OurTeam.Opponent()])
-            {
-                return new AgentActivityData(AgentStatusCode.RequestRemovementOpponentTile, coordinate);
-            }
+            if (Calc.Field[coordinate].IsTileOn[OurTeam.Opponent()])return new AgentActivityData(AgentStatusCode.RequestRemovementOpponentTile, coordinate);
             return new AgentActivityData(AgentStatusCode.RequestMovement, coordinate);
-
         }
     }
 }
