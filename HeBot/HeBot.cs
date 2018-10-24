2d0b151cfed1a9c3b262b5d45a78e8ec4931ef70
Seita Higashi <higashi110902@gmail.com>
2018-10-24 14:58:56 +0900

    BestHand obsolete

--- HeBot/HeBot.cs
+++ HeBot/HeBot.cs
@@ -23,25 +23,30 @@ namespace nitkagoshima_sysken.Procon29.HeBot
         {
             var result = new AgentActivityData[2];
             var maxpoint = int.MinValue;
+            var nowpoint = 0;
             var agentActivityData = new AgentActivityData[2];
+            var c = new Calc();
+            var destinationOne = new Coordinate();
+            var destinationTwo = new Coordinate();
 
             result = BestHand(Calc, 1).AgentActivityData;
             foreach (Arrow arrowOne in Enum.GetValues(typeof(Arrow)))
             {
-                var destinationOne = calc.Agents[OurTeam, AgentNumber.One].Position + arrowOne; if (!calc.Field.CellExist(destinationOne)) continue;
+                destinationOne = calc.Agents[OurTeam, AgentNumber.One].Position + arrowOne; if (!calc.Field.CellExist(destinationOne)) continue;
 
                 foreach (Arrow arrowTwo in Enum.GetValues(typeof(Arrow)))
                 {
-                    var destinationTwo = calc.Agents[OurTeam, AgentNumber.Two].Position + arrowTwo;
+                    destinationTwo = calc.Agents[OurTeam, AgentNumber.Two].Position + arrowTwo;
                     if (!calc.Field.CellExist(destinationTwo)) continue;
                     if (destinationOne == destinationTwo) continue;
                     agentActivityData[(int)AgentNumber.One] = MoveOrRemoveTile(destinationOne);
                     agentActivityData[(int)AgentNumber.Two] = MoveOrRemoveTile(destinationTwo);
-                    var c = calc.Simulate(OurTeam, agentActivityData);
+                    c = calc.Simulate(OurTeam, agentActivityData);
                     foreach (var item in agentActivityData) item.AgentStatusData.ToRequest();
-                    if (maxpoint < (c.Field.TotalPoint(OurTeam) - c.Field.TotalPoint(OurTeam.Opponent())))
+                    nowpoint = c.Field.TotalPoint(OurTeam) - c.Field.TotalPoint(OurTeam.Opponent());
+                    if (maxpoint < nowpoint)
                     {
-                        maxpoint = c.Field.TotalPoint(OurTeam) - c.Field.TotalPoint(OurTeam.Opponent());
+                        maxpoint = nowpoint;
                         result = agentActivityData.DeepClone();
                     }
                 }
