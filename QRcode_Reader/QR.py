f8c5d414f5ff64ff09d89350b5f3aad036225ff8
Glaz Egy <rei.izayoi.0804@gmail.com>
2018-10-27 20:44:21 +0900

    Update

--- QRcode_Reader/QR.py
+++ QRcode_Reader/QR.py
@@ -7,33 +7,37 @@ import cv2
 
 cap = cv2.VideoCapture(0)
 
+
+def decodes():
+    image = 'photo.png'
+    data = decode(Image.open(image))
+    try:
+        outt = data[0][0].decode('utf-8', 'ignore')
+    except:
+        return False
+    outt = outt.split(':')
+    y, _ = outt[0].strip().split(' ')
+    outt.remove('')
+    print(outt)
+    if len(outt) == int(y)+3:
+        with open('output.pqr','w') as f:
+            for i in range(len(outt)-1):
+                f.write(outt[i].strip()+':'+'\n')
+                print(outt[i].strip()+':')
+        Flag = True
+    else:
+        Flag = False
+    return Flag
+
 while(True):
     ret, frame = cap.read()
-
     cv2.imshow('frame', frame)
-
     key = cv2.waitKey(1) & 0xFF
-
     if key == ord('q'):
         break
     if key == ord('s') or key == ord(' '):
         path = 'photo.png'
         cv2.imwrite(path, frame)
-        break
+        if decodes():
+            break
 
-
-# QRコード(QRcode.png)の指定
-image = 'photo.png'
-# QRコードの読取り
-data = decode(Image.open(image))
-# コード内容テキストファイル(output.txt)に出力
-f = open('output.pqr','w')
-# utf-8にデコードして出力
-outt = data[0][0].decode('utf-8', 'ignore')
-#outt = text.replace(' ',',')
-outt = outt.split(':')
-for i in range(len(outt)-1):
-    f.write(outt[i].strip()+':'+'\n')
-    print(outt[i].strip()+':')
-
-f.close()
